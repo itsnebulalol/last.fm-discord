ceb4f8f12c42a29e77985e2146a1ff669420260f
Nebula <nebulalol@icloud.com>
2022-06-03 15:28:29 +0000

    wip progress bar

--- rpc.py
+++ rpc.py
@@ -1,5 +1,6 @@
 from pypresence import Presence
 import datetime
+import time
 
 client_id = '982286050858795008'
 presence = Presence(client_id)
@@ -8,6 +9,26 @@ already_disabled = True
 start_time = None
 last_track = None
 
+def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
 def enable_rpc():
     global already_enabled,already_disabled
@@ -43,6 +64,11 @@ def update_status(track, album, time_remaining):
                 presence.update(details=trackList[1], state=trackList[0],
                     large_image='icon', large_text='Last.fm')
 
+        printProgressBar(0, float(time_remaining) + start_time, prefix = 'Progress:', suffix = 'Complete', length = 50)
+        for i in range(1, float(time_remaining) + start_time + 1):
+            time.sleep(1)
+            # Update Progress Bar
+            printProgressBar(i + 1, float(time_remaining) + start_time, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
 def disable_rpc():
     global already_enabled
